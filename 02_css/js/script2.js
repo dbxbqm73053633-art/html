f2587f8fc6fee7122a65ea331258b248fd520421
dbxbqm73053633-art <dbxbqm73053633@gmail.com>
2025-12-24 16:55:05 +0900

    Add files via upload

---
+++ 02_css/js/script2.js
@@ -0,0 +1,10 @@
+$(function(){
+$("li").css({"list-style":"none","box-sizing":"border-box","justify-content":"center"})
+$("ul").css({"border":"3px solid #000","width":880,"height":200, "margin":"0 auto",
+    "display":"flex","align-items":"center","gap":20,})
+$("ul .aa").css({"width":200,"height":180,"border":"1px solid #000","background":"red"})
+$("ul .bb").css({"width":200,"height":180,"border":"1px solid #000","background":"darkgoldenrod"})
+$("ul .cc").css({"width":200,"height":180,"border":"1px solid #000","background":"yellow"})
+$("ul .dd").css({"width":200,"height":180,"border":"1px solid #000","background":"green"})
+
+})
