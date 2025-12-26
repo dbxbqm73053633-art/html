b0a639d6b79b3ef0861bc473ad641965862948bc
dbxbqm73053633-art <dbxbqm73053633@gmail.com>
2025-12-26 17:26:25 +0900

    Add files via upload

--- 03_ibk(familysite)/js/script.js
+++ 03_ibk(familysite)/js/script.js
@@ -7,11 +7,14 @@ $(".family .family_list").hide()
 // 1번째 패밀리 버튼을 클릭하면 fmailylist가 보임 / list 안보임
 $(".family button").click(function(){
     $(".family .family_list").toggle()
+    $(".family2 .family_list").hide()
 })
 
 // 2번째 패밀리 버튼을 클릭하면 fmailylist가 보임 / list 안보임
 $(".family2 button").click(function(){
     $(".family2 .family_list").toggle()
+    $(".family .family_list").hide()
 })
 
 })
+
