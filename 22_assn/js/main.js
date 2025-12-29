89c4af25727d5f3dd12976fde5fe2ab5d8d140e7
dbxbqm73053633-art <dbxbqm73053633@gmail.com>
2025-12-29 12:40:25 +0900

    Add files via upload

---
+++ 22_assn/js/main.js
@@ -0,0 +1,26 @@
+$(function(){
+    $('.slickslide').slick({
+        //  슬라이드 자동실행
+    autoplay:true,
+    // 페이지버튼 보이기
+    dots:true,
+    // 자동실행 시간조절
+    autoplaySpeed:3000,
+    // 좌우버튼
+    arrows:false,
+    // fade효과
+    // fade:true,
+    // 슬라이드효과를 상하
+    // vertical:true,
+    // 반복하지 않고 1번만 실행
+    // infinite:false,
+    // 슬라이드 움직임 멈춤
+    pauseOnHover:false,
+    // 슬라이드 여러개 움직임
+    slidesToScroll:1,
+    
+
+
+  });
+
+})
